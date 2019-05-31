1937bb2693a98071723104e9c5c75a74a9f69a6d
Jonathan Morales <Jonathan@jonathans-mbp.attlocal.net>
2019-05-31 09:48:12 -0400

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
+app.get('*', (res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!')
 });
