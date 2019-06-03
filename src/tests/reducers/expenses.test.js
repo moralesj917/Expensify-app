2151ad04aaebe63ba8435c4bb23878fc8dc3d4ca
Jonathan Morales <Jonathan@jonathans-mbp.attlocal.net>
2019-06-03 10:49:59 -0400

    Fetching data from database

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -62,3 +62,12 @@ test('should not edit expense if expense not found', () => {
   const state = expensesReducer(expenses, action);
   expect(state[0].description).toBe('Gum');
 });
+
+test('should set expenses', () => {
+  const action = {
+    type: 'SET_EXPENSES',
+    expenses: [expenses[1]]
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[1]]);
+});
